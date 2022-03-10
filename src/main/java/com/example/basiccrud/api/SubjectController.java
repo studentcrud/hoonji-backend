a0e70c5642cbba1dd731dde0ee4f20cbf6e3f0ba
sunghoonkingg <shkim1199@e8ight.co.kr>
2022-03-10 17:18:54 +0900

    자유로운 테스트

---
+++ src/main/java/com/example/basiccrud/api/SubjectController.java
@@ -0,0 +1,55 @@
+package com.example.basiccrud.api;
+
+import com.example.basiccrud.domain.Subject;
+import com.example.basiccrud.dto.SubjectRequestDto;
+import com.example.basiccrud.repository.SubjectRepository;
+import com.example.basiccrud.service.SubjectService;
+import com.example.basiccrud.utills.PagingResult;
+import com.example.basiccrud.utills.Wrapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
+public class SubjectController {
+
+    private final SubjectService subjectService;
+    private final SubjectRepository subjectRepository;
+
+
+
+    //과목 저장
+    @PostMapping("/subjects")
+    public void createSubject(@RequestBody SubjectRequestDto subjectRequestDto){
+        subjectService.setSubject(subjectRequestDto);
+    }
+
+    //과목 조회 페이징
+    @GetMapping("/subjects/{curPage}")
+    public PagingResult readSubject(@PathVariable Integer curPage){
+        return subjectService.getSubject(curPage);
+    }
+
+    //과목 조회 전체
+    @RequestMapping(value = "/subjects",method=RequestMethod.GET)
+    public List<Subject> readSubjects(){
+        return subjectRepository.findAll();
+    }
+
+    //과목 변경
+    @PutMapping("/subjects/{id}")
+    public String updateSubject(@PathVariable Long id,@RequestBody SubjectRequestDto subjectRequestDto){
+        return subjectService.updateSubject(id,subjectRequestDto);
+    }
+
+    //과목 삭제
+    @DeleteMapping("/subjects")
+    public void deleteSubject(@RequestBody Wrapper subjects){
+        subjectService.deleteSubject(subjects);
+    }
+
+
+}
---
+++ src/main/java/com/example/basiccrud/dto/responseDto/ProfessorResponseDto.java
@@ -0,0 +1,25 @@
+package com.example.basiccrud.dto.responseDto;
+
+
+import com.example.basiccrud.domain.Professor;
+import com.example.basiccrud.domain.Subject;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProfessorResponseDto {
+
+    private Long id;
+    private String professorName;
+    private int professorAge;
+    private String subjectName;
+
+
+    public ProfessorResponseDto(Long id,String professorName, int professorAge, String subjectName){
+        this.id = id;
+        this.professorName = professorName;
+        this.professorAge = professorAge;
+        this.subjectName = subjectName;
+    }
+}
